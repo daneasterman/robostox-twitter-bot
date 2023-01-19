59cccc3fc2c6f81c63a1f65466d150414af725b1
Daniel Easterman <daniel.easterman@gmail.com>
2023-01-19 11:13:31 +0000

    Change CIK

--- main.py
+++ main.py
@@ -28,7 +28,7 @@ def get_filing():
 	print('**MESSAGE COUNTER**', message_counter)
 	SEC_URL = "https://www.sec.gov/cgi-bin/browse-edgar?action=getcurrent&CIK=&type=&company=&dateb=&owner=include&start=0&count=40&output=atom"
 	# TSLA_CIK = "0001318605"
-	DUMMY_CIK = "0001543151"
+	DUMMY_CIK = "0001866226"
 	headers = {'User-agent': 'Mozilla/5.0'}
 	try:
 		response = requests.get(SEC_URL, headers=headers)
